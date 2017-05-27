5152f29f4db94bec47df9790931ac295daafc69c
pividmonrer <sergiodavidpj@hotmail.com>
2017-05-27 18:51:01 -0500

    Añadida clase para la conexion con Oracle

---
+++ questo/src/java/MD/conexion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author sergi
+ */
+public class conexion {
+        Connection conexion = null;
+    
+    public Connection conectar()
+    {
+        try
+        {
+          Class.forName("oracle.jdbc.driver.OracleDriver");
+          conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "12345");
+          System.out.println("Conectado!!");
+        }catch(Exception ex)
+        {
+            System.out.println("ERROR:"+ex.getMessage());
+        }
+        return conexion;
+    }
+    public static void main (String args[])
+    {
+        conexion c = new conexion();
+        System.out.println(""+c.conectar());
+    }
+}
